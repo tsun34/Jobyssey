d3e36055bcd28ac67b2d22ed23efb517643c4b5b
CharSun <tingxiao.sun@gmail.com>
2020-09-01 10:07:07 -0700

    add logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import { HashRouter } from 'react-router-dom';
 import App from './App'
 import {Provider} from 'react-redux'
 import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
 
 const Root = ({ store}) => (
   <Provider store={ store }>
@@ -14,7 +17,23 @@ const Root = ({ store}) => (
 );
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore({});
+  let store; 
+  
+  if (localStorage.jwtToken){
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser} };
+    store = configureStore({});
+    const currentTime = Date.now() / 1000;
+
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  }else {
+    store = configureStore({});
+  }
+  
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import uiReducer from './ui_reducer';
 import modalReducer from './modal_reducer';
+import session from './session_reducer';
 
 const rootReducer = combineReducers({
+    session,
     ui: uiReducer,
 });
 
