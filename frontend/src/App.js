553917394136f851aad30cfac7319dd8e66a5a20
Philip Tynan <tynan_philip@me.com>
2020-09-02 15:38:27 -0700

    search function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,9 @@ import { Switch, Route } from 'react-router-dom';
 import GreetingContainer from './components/greeting/greeting_container'
 
 import ModalContainer from './components/modal/modal_container';
-import LangindPage from './components/landing_page/landing_page';
+import LandingPage from './components/landing_page/landing_page';
 import './css/page.css';
+import SearchPage from "./components/search_test/search";
 // import NavBarContainer from './nav/navbar_container';
 
 import HomePageContainer from './components/homePage/home_page_container';
@@ -14,10 +15,9 @@ import HomePageContainer from './components/homePage/home_page_container';
 const App = () => (
   <div className='slant-display'>
     <Switch>
-
+      <Route path="/search" component={SearchPage} />
       <AuthRoute exact path="/" component={HomePageContainer} />
       <ProtectedRoute exach path="/home" component={LandingPage} />
-      
     </Switch>
     <ModalContainer />
   </div>
