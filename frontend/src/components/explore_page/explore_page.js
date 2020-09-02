fa7dfd10f9710cbfe3c8cd9176874a5ec441bcf5
maggieyao1211 <momokoyao@hotmail.com>
2020-09-02 11:13:53 -0700

    Add search container to explore page

--- frontend/src/components/explore_page/explore_page.js
+++ frontend/src/components/explore_page/explore_page.js
@@ -3,6 +3,8 @@ import GreetingContainer from '../greeting/greeting_container';
 import '../../css/explore-page.css';
 import ExploreSearchBar from './explore_search_bar';
 import ExploreCompanyList from './explore_company_list';
+import { FcSearch } from 'react-icons/fc';
+import { MdSettingsVoice } from 'react-icons/md';
 
 class LandingPage extends React.Component {
 
