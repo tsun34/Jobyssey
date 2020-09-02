cc524911a85b941809c9cd8338dfa9a4a3f5dacd
maggieyao1211 <momokoyao@hotmail.com>
2020-09-01 22:39:57 -0700

    Add user icon & dropdown for logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import GreetingContainer from './components/greeting/greeting_container'
 
 import ModalContainer from './components/modal/modal_container';
 import LandingPage from './components/landing_page/landing_page';
-
 import './css/page.css';
 // import NavBarContainer from './nav/navbar_container';
 
@@ -15,10 +14,8 @@ import HomePageContainer from './components/homePage/home_page_container';
 const App = () => (
   <div className='slant-display'>
     <Switch>
-
       <AuthRoute exact path="/" component={HomePageContainer} />
       <ProtectedRoute exach path="/home" component={LandingPage} />
-      
     </Switch>
     <ModalContainer />
   </div>
