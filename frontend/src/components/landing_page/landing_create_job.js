81864f8689e7255f1353210c52318099999b2012
maggieyao1211 <momokoyao@hotmail.com>
2020-09-02 15:40:01 -0700

    jobs

--- frontend/src/components/landing_page/landing_create_job.js
+++ frontend/src/components/landing_page/landing_create_job.js
@@ -9,9 +9,12 @@ class LandingCreateJob extends React.Component {
             companyName: '',
             position: '',
             category: '',
+            applicationLink: '',
         };
         this.onCompanyNameChange = this.onCompanyNameChange.bind(this);
         this.onPositionChange = this.onPositionChange.bind(this);
+        this.onApplicationLinkChange = this.onApplicationLinkChange.bind(this);
+        this.onSubmitClick = this.onSubmitClick.bind(this);
     }
 
     onCompanyNameChange(e) {
@@ -24,8 +27,25 @@ class LandingCreateJob extends React.Component {
         this.setState({position: e.currentTarget.value});
     }
 
+    onApplicationLinkChange(e) {
+        e.preventDefault();
+        this.setState({applicationLink: e.currentTarget.value});
+    }
+
+    onSubmitClick(e) {
+        e.preventDefault();
+        const { companyName, position, category, applicationLink } = this.state;
+        this.props.composeJob({
+            company_name: companyName,
+            position_name: position,
+            stage: category,
+            application_link: applicationLink,
+        }).then(() => this.props.closeModal());
+    }
+
     render() {
-        const { companyName, position, category } = this.state;
+        const { companyName, position, category, applicationLink } = this.state;
+        console.log(this.props);
         return (
             <div className="sign-container">
                 <form className="sign-form">
@@ -45,6 +65,14 @@ class LandingCreateJob extends React.Component {
                         value={position}
                         required
                     />
+                    <input
+                        className="create-job-input"
+                        onChange={this.onApplicationLinkChange}
+                        placeholder="Application Link *"
+                        type="text"
+                        value={applicationLink}
+                        required
+                    />
                     <div className="landing-menu-container">
                         <input
                             className="sign-input"
@@ -54,13 +82,13 @@ class LandingCreateJob extends React.Component {
                             required
                         />
                         <div className="landing-menu-dropdown">
-                            <div className="landing-menu-row" onClick={e => this.setState({category: 'Wishlist'})}>Wishlist</div>
-                            <div className="landing-menu-row" onClick={e => this.setState({category: 'Applied'})}>Applied</div>
-                            <div className="landing-menu-row" onClick={e => this.setState({category: 'Interview'})}>Interview</div>
-                            <div className="landing-menu-row" onClick={e => this.setState({category: 'Offer'})}>Offer</div>
+                            <div className="landing-menu-row" onClick={e => this.setState({category: 'wishlist'})}>Wishlist</div>
+                            <div className="landing-menu-row" onClick={e => this.setState({category: 'applied'})}>Applied</div>
+                            <div className="landing-menu-row" onClick={e => this.setState({category: 'interview'})}>Interview</div>
+                            <div className="landing-menu-row" onClick={e => this.setState({category: 'offer'})}>Offer</div>
                         </div>
                     </div>
-                    <button className="sign-button" type="submit">Create Job</button>
+                    <button className="sign-button" onClick={this.onSubmitClick} type="submit">Create Job</button>
                 </form>
             </div>
         );
