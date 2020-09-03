4fa4d702f20cafcc887f71809127432dc1ab91f3
maggieyao1211 <momokoyao@hotmail.com>
2020-09-03 13:28:27 -0700

    review page initial layout

--- frontend/src/components/review/review_page_container.js
+++ frontend/src/components/review/review_page_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import ReviewPage from './review_page';
+<<<<<<< HEAD
 import { fetchCompanyReviews, postReview } from '../../actions/review_actions';
 import { fetchCompany } from '../../actions/company_action';
 
@@ -8,13 +9,25 @@ const mstp = ({reviews, companies, session}) => {
         reviews: Object.values(reviews.all) ?? [],
         company: companies.show,
         currentUserId: session.user.id,
+=======
+import { fetchCompanyReviews } from '../../actions/review_actions';
+import { fetchCompany } from '../../actions/company_action';
+
+const mstp = ({reviews, companies}) => {
+    return {
+        reviews: Object.values(reviews.all) ?? [],
+        company: companies.show,
+>>>>>>> review page initial layout
     };
 };
 
 const mdtp = dispatch => {
     return {
         fetchCompanyReviews: companyId => dispatch(fetchCompanyReviews(companyId)),
+<<<<<<< HEAD
         postReview: data => dispatch(postReview(data)),
+=======
+>>>>>>> review page initial layout
         fetchCompany: companyId => dispatch(fetchCompany(companyId)),
     };
 };
--- frontend/src/reducers/reviews_reducer.js
+++ frontend/src/reducers/reviews_reducer.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { RECEIVE_COMPANY_REVIEWS, RECEIVE_NEW_REVIEW } from '../actions/review_actions';
+=======
+import { RECEIVE_COMPANY_REVIEWS } from '../actions/review_actions';
+>>>>>>> review page initial layout
 
 const companiesReducer = (state = { all: {} }, action) => {
     Object.freeze(state);
@@ -7,9 +11,12 @@ const companiesReducer = (state = { all: {} }, action) => {
       case RECEIVE_COMPANY_REVIEWS:
         newState.all = action.reviews.data;
         return newState;
+<<<<<<< HEAD
       case RECEIVE_NEW_REVIEW:
         newState.all = [...newState.all, action.review.data];
         return newState;
+=======
+>>>>>>> review page initial layout
       default:
         return state;
     }
