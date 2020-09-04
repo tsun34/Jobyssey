b774bb3c2b44433758cc3e357ad1ae7f0cc34c83
xiao <yuxiao08322@gmail.com>
2020-09-03 17:55:29 -0700

    fix conflict

--- frontend/src/components/job_index/job_index_item.jsx
+++ frontend/src/components/job_index/job_index_item.jsx
@@ -4,81 +4,102 @@ import '../../css/job_index_item.css'
 class JobIndexItem extends React.Component {
     constructor(props) {
         super(props);
+        this.onClick = this.onClick.bind(this)
+    }
+
+    onClick() {
+        console.log(this.props)
+        this.props.removeUserJob(this.props.job._id).then(this.props.closeModal())
     }
 
     render() {
         const job = this.props.job
         return (
-        <div className='job-index'>
-            <div className='job-index-top'>
-                <ul className='job-index-top-left'>
-                    <li className='li-details'>
-                        <p className='li-title'>Company Name:</p> 
-                        <div className='li-field'>{job.company_name}</div>
+        <div className='job-index-item'>
+            <button onClick={this.onClick}>Delete Job</button>
+                <ul >
+                    <li className='li-details'>company_name: 
+                        {job.company_name}
                     </li >
-                    <li className='li-details'>
-                        <p className='li-title'>Position Name:</p> 
-                        <div className='li-field'>{job.position_name}</div>
+                    <li className='li-details'>position_name: 
+                        {job.position_name}
                     </li>
-                    <li className='li-details'>
-                        <p className='li-title'>Stage:</p> 
-                        <div className='li-field'>{job.stage}</div>
+                    <li className='li-details'>stage: 
+                        {job.stage}
                     </li>
-                    <li className='li-details'>
-                        <p className='li-title'>Application Link:</p> 
-                        <div className='li-field'>{job.application_link}</div>
+                    <li className='li-details'>application_link: 
+                        {job.application_link}
                     </li>
-                    <li className='li-details'>
-                        <p className='li-title'>Salary:</p>
-                        <div className='li-field'>{job.salary}</div>
-                    </li>
-                    <li className='li-details'>
-                        <p className='li-title'>Location:</p>
-                        <div className='li-field'>{job.location}</div>
-                    </li>
-                </ul>
-                <ul className='job-index-top-right'>
-                    <li className='li-details'>
-                        <p className='li-title'>Deadline:</p>
-                        <div className='li-field'>{job.deadline}</div>
-                    </li>
+                    {
+                        job.salary && 
+                        <li className='li-details'>salary:
+                            {job.salary}
+                        </li>
+                    }
+                    {
+                        job.location &&
+                        <li className='li-details'>location:
+                            {job.location}
+                        </li>
+                    }
 
-                    <li className='li-details'>
-                        <p className='li-title'>Applied On:</p>
-                        <div className='li-field'>{job.date_applied}</div>
-                    </li>
+                    {
+                        job.deadline &&
+                        <li className='li-details'>deadline:
+                            {job.deadline}
+                        </li>
+                    }
+                    
+                    {
+                        job.date_applied &&
+                        <li className='li-details'>date_applied:
+                            {job.date_applied}
+                        </li>
+                    }
 
-                    <li className='li-details'>
-                        <p className='li-title'>Interview:</p>
-                        <div className='li-field'>{job.interview_date}</div>
-                    </li>
+                    {
+                        job.interview_date &&
+                        <li className='li-details'>interview_date:
+                            {job.interview_date}
+                        </li>
+                    }
 
-                    <li className='li-details'>
-                        <p className='li-title'>Heard back:</p>
-                        <div className='li-field'>{job.decision_date}</div>
-                    </li>
+                    {
+                        job.decision_date &&
+                        <li className='li-details'>decision_date:
+                            {job.decision_date}
+                        </li>
+                    }
+                    
+                    {
+                        job.decision &&
+                        <li className='li-details'>decision:
+                            {job.decision}
+                        </li>
+                    }
 
-                    <li className='li-details'>
-                        <p className='li-title'>Decision:</p>
-                        <div className='li-field'>{job.decision}</div>
-                    </li>
-                </ul>
-            </div>
-            <ul className='job-index-bottom'>
-                <li className='li-textbox'>
-                    <p className='li-title'>Notes:</p>
-                    <div className='li-textfield'>{job.notes}</div>
-                </li>
-                <li className='li-textbox'>
-                    <p className='li-title'>Network Contacts:</p>
-                    <div className='li-textfield'>{job.networking_contacts}</div>
-                </li>
-                <li className='li-textbox'>
-                    <p className='li-title'>Study Prep:</p>
-                    <div className='li-textfield'>{job.studying_preparation}</div>
-                </li>
-            </ul>                    
-            
+                    {
+                        job.notes &&
+                        <li className='li-details'>notes:
+                            {job.notes}
+                        </li>
+                    }
+
+                    {
+                        job.networking_contacts &&
+                        <li className='li-details'>networking_contacts:
+                            {job.networking_contacts}
+                        </li>
+                    }
+                    
+                    {
+                        job.studying_preparation &&
+                        <li className='li-details'>studying_preparation:
+                            {job.studying_preparation}
+                        </li>
+                    }
+                    
+            </ul>
         </div>
         )
     }
