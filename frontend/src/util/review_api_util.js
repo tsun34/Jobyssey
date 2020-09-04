eea1276624c3d6e8b4b9d275b534848d7d4dd56f
maggieyao1211 <momokoyao@hotmail.com>
2020-09-03 19:47:06 -0700

    fix some errors after rebase

--- frontend/src/util/review_api_util.js
+++ frontend/src/util/review_api_util.js
@@ -6,8 +6,4 @@ export const getCompanyReviews = companyId => {
 
 export const createReview = data => {
   return axios.post(`/api/reviews/${data.companyId}`, data);
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> review page complete
