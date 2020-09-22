e465f51bf1b9b9b01e5b4972306af46d994d7b34
CharSun <tingxiao.sun@gmail.com>
2020-09-22 13:11:49 -0700

    update splash page

--- frontend/src/components/homePage/home_page.jsx
+++ frontend/src/components/homePage/home_page.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../pics/jobyssey_logo.png'
+import logo from "../../pics/jobyssey-logo.png";
+import homeImg1 from "../../pics/1.png";
+import homeImg2 from "../../pics/2.png";
+import homeImg3 from "../../pics/3.png";
+import homeImg4 from "../../pics/4.png";
+import homeImg5 from "../../pics/5.png";
+
 import '../../css/homepage.css'
-import test1 from '../../pics/test1.png'
-import personal_job_task from '../../pics/personal_job_task.png'
-import visual from '../../pics/visual.png'
-import explore from '../../pics/explore.png' 
+
 import profilepic from '../../pics/profilepic.png'
 
 class HomePage extends React.Component {
@@ -36,7 +39,7 @@ class HomePage extends React.Component {
 
 
     render() {
-    return <div>
+    return <>
         <nav className='home-nav'>
             <ul className='home-nav-info'>
                 <li><Link to='/'> <img className='home-nav-logo' src={logo} alt="logo"/></Link></li>          
@@ -52,68 +55,78 @@ class HomePage extends React.Component {
         </nav>
 
         <section className='jobyssey-home-section'>
-            <div>
-                <h1 className='jobyssey-home-title'>Jobyssey helps you to presue your goals</h1>
-                <span className='jobyssey-home-body'>Stay on track</span>
+            <div className='jobyssey-home-text'>
+                <h1 className='jobyssey-home-title'>Jobyssey helps you to find your dream job and achieve your goals.</h1>
+                <span className='jobyssey-home-body'>Jobyssey's boards, lists and cards enable job seekers to organize and prioritize applications in a fun, flexiable and rewarding way.</span>
                 <p className='jobyssey-home-note' >Need to create an account? <span onClick={this.onSignUpClick} className='jobyssey-home-note-link'> Get started</span></p>
                 <p className="jobyssey-home-note-link" onClick={this.handleDemo}>Try with Demo Login</p>
             </div>
-            <img className='jobyssey-home-img' src={test1} alt='jobyssey homepage'/>
+            <img className='jobyssey-home-img' src={homeImg1} alt='jobyssey homepage'/>
         </section>  
 
-        <section className='personal-task-section'>
-            <div className='jobyssey-home-box'>
-                <img className='jobyssey-task-img' src={personal_job_task} alt='jobyssey homepage' />
-                <span className='jobyssey-task-text'>
-                    <h1>Stay organized</h1>
-                    <span className='jobyssey-task-note'>
-                        Jobyssey keeps track of every detail of your job application from application deadline and job description to networking contacts and preparation notes. 
-                    </span>
+        <section className='personal-task-section'>       
+            <img className='jobyssey-task-img' src={homeImg2} alt='jobyssey homepage' />
+            <span className='jobyssey-task-text'>
+                <h1>Designed for Everyone</h1>
+                <span>
+                    Whether you are fresh out of college or a professional with many years of experience, Jobyseey helps you stay organized. 
                 </span>
-            </div>
+            </span>
         </section>
         <section className='personal-task-section'>
-            <div className='jobyssey-home-box'>
-                <span className='visual-task-text'>
-                    <h1>Visual your success</h1>
-                    <span className='jobyssey-task-note'>
-                        Jobyseey incorporates the d3-sankey library and generates personalized sankey diagrams to easily visualize the progress of job searching you have accomplished. 
-                    </span>
+            <span className='jobyssey-task-text'>
+                <h1>Information at a Glance</h1>
+                <span>
+                    Dive into the details from application deadline and job description to networking contacts and preparation notes, and more directly to Jobyseey cards.
                 </span>
-                <img className='jobyssey-visual-img' src={visual} alt='jobyssey homepage' />
-            </div>
+            </span>
+            <img className='jobyssey-visual-img' src={homeImg3} alt='jobyssey homepage' />
         </section>
         <section className='personal-task-section'>
-            <div className='jobyssey-home-box'>
-                <img className='jobyssey-task-img' src={explore} alt='jobyssey homepage' />
-                <span className='jobyssey-task-text'>
-                    <h1>Explore & Share</h1>
-                    <span className='jobyssey-task-note'>
-                        Jobyssey is a safe place for job seekers to share their interviewing experiences. Users can search and view other job seekers’ experience at different companies.
-                    </span>
+            <img className='jobyssey-visual-img' src={homeImg4} alt='jobyssey homepage' />
+            <span className='jobyssey-task-text'>
+                <h1>Visual your success</h1>
+                <span>
+                    Jobyseey incorporates the d3-sankey library and generates personalized sankey diagrams to easily visualize the progress of job searching you have accomplished.
                 </span>
-            </div>
+            </span>
         </section>
-        <h1 className='contributor-our-team' >Our Team</h1>
-        <section className='contributor-section'>
-            <div className='contributor-div'>
-                <img className='contributor-img' src={profilepic} />
-                <h3>Xiao Yu</h3>
-            </div>
-            <div className='contributor-div'>
-                <img className='contributor-img' src={profilepic} />
-                <h3>Charlotte Sun</h3>
-            </div>
-            <div className='contributor-div'>
-                <img className='contributor-img' src={profilepic} />
-                <h3>Maggie Yao</h3>
-            </div>
-            <div className='contributor-div'>
-                <img className='contributor-img' src={profilepic} />
-                <h3>Phillip Tynan</h3>
-            </div>
+        <section className='personal-task-section'>
+            <span className='jobyssey-task-text'>
+                <h1>Your Data Belongs to You</h1>
+                <span>
+                    Jobyseey offers the option to export all your data as an Excel or csv file of your choice. So you can easily transport, share and manipulate your information however you want.  
+                </span>
+            </span>
+            <img className='jobyssey-visual-img' src={homeImg5} alt='jobyssey homepage' />
         </section>
-        <div className='footer'>
+
+        <section className='personal-task-section'>
+            <img className='jobyssey-visual-img' src={homeImg5} alt='jobyssey homepage' />
+            <span className='jobyssey-task-text'>
+                <h1>Explore and Share</h1>
+                <span>
+                    Jobyssey is a safe place for job seekers to share their interviewing experiences. Users can also search and view other job seekers’ experience at different companies on our explore page.
+                </span>
+            </span>
+        </section>
+
+        <section className='join-today-section'>
+            <h1>Start Organizing Today</h1>
+            <span>Sign up and join job seekers worldwide who are using Jobyseey to find their dream jobs.</span>
+            <button onClick={this.onSignUpClick}>Get Started - It's Free!</button>
+        </section>
+
+        <section className='contact-us'>
+            <a href="https://github.com/tsun34"> <i className='fab fa-github'></i> Charlotte Sun</a>
+            <a href="https://github.com/maggieyao1211"> <i className='fab fa-github'></i> Maggie Yao</a>
+            <a href="https://github.com/xiaoyu322"> <i className='fab fa-github'></i> Phillip Tynan</a>
+            <a href="https://github.com/tynan415"> <i className='fab fa-github'></i> Xiao Yu</a>
+
+        </section>
+
+
+        {/* <div className='footer'>
             <div className='footer-content' >
                 <span>About</span>
                 <span>Privacy & Terms</span>
@@ -122,10 +135,10 @@ class HomePage extends React.Component {
             </div>
             <Link to='/'> <img className='footer-logo' src={logo} alt="logo" /></Link>
             <div>
-                <button className='back-to-top-btn' onClick={this.onBackToTopClick}> Back To Top</button>
             </div>
-        </div>
-    </div>
+        </div> */}
+        <div className='back-to-top-btn' onClick={this.onBackToTopClick}> <i className='fas fa-arrow-up'></i></div>
+    </>
     }
 };
 
